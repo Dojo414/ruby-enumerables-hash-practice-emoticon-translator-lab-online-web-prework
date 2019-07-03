9d1363b8025943ab3536d829aee809196917d0e8
Donovan Jones <donovanljones94@gmail.com>
2019-07-03 14:25:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,10 +20,10 @@ end
 def get_japanese_emoticon(file_name, emoticon)
   translator = load_library(file_name)
   translator.each do |upper_level, inner_emote|
-    if upper_level === "get_emoticon"
-     
+    if upper_level === "get_emoticon" 
+      if upper_level.has_key? (emoticon)
+        answer = upper_level[emoticon]
           binding.pry
-        end
       end
     end
   end
