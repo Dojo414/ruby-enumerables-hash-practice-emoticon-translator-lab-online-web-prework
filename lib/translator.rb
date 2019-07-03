feb7439b136cd9a75438390d4e430d48c05ccb2c
Donovan Jones <donovanljones94@gmail.com>
2019-07-03 14:04:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,20 @@
 # require modules here
 
-def load_library
-  # code goes here
+require 'yaml'
+require 'pry'
+
+def load_library(file)
+  emoticons = YAML.load_file(file)
+  translator = { "get_meaning" => {}, "get_emoticon" => {}}
+  emoticons.each do |writing, emotes|
+    translator.each do |outer_key, inner_hash|
+      if outer_key === "get_meaning"
+        inner_hash[emotes[1]] = writing
+      else inner_hash[emotes[0]] = emotes[1]
+      end 
+    end
+  end
+  translator
 end
 
 def get_japanese_emoticon
